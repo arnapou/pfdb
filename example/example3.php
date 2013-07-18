90ee37b209056891773e98144998844afc9a0e07
Arnaud Buathier <arnaud.buathier@livingobjects.fr>
2013-07-18 15:56:34 +0200

    adding copyright informations

--- example/example3.php
+++ example/example3.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Arnapou PFDB package.
+ *
+ * (c) Arnaud Buathier <arnaud@arnapou.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Arnapou\PFDB\Condition\ConditionBuilder;
 
 include __DIR__ . '/functions.php';
