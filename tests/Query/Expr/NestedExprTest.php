ab224ca1427b14fd7b94e8e9c633055e9320a4b4
Arnaud Buathier <arnaud@arnapou.net>
2024-11-25 08:53:40 +0100

    support php 8.4

--- tests/Query/Expr/NestedExprTest.php
+++ tests/Query/Expr/NestedExprTest.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Arnapou\PFDB\Tests\Query;
+namespace Arnapou\PFDB\Tests\Query\Expr;
 
 use Arnapou\PFDB\Query\Helper\ExprHelperTrait;
 use PHPUnit\Framework\TestCase;
